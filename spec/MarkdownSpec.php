f3bf6e445c797af240b47c6668ff518517c35b06
matvey <artemiy@pulyavin.ru>
2015-02-22 12:33:32 +0300

    Commit

---
+++ spec/MarkdownSpec.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace spec;
+
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+class MarkdownSpec extends ObjectBehavior
+{
+
+    public function it_should_return_false_when_over_3999() {
+    	$rand = rand(3999, 100000);
+
+    	$this->getRoman($rand)->shouldBe(false);
+    }
+
+    public function it_should_return_I() {
+    	$this->getRoman(1)->shouldBe("I");
+    }
+
+    public function it_should_return_II() {
+    	$this->getRoman(2)->shouldBe("II");
+    }
+
+    public function it_should_return_III() {
+    	$this->getRoman(3)->shouldBe("III");
+    }
+
+    public function it_should_return_IV() {
+    	$this->getRoman(4)->shouldBe("IV");
+    }
+
+    public function it_should_return_V() {
+    	$this->getRoman(5)->shouldBe("V");
+    }
+
+    public function it_should_return_VIII() {
+    	$this->getRoman(8)->shouldBe("VIII");
+    }
+
+    public function it_should_return_IX() {
+    	$this->getRoman(9)->shouldBe("IX");
+    }
+
+    public function it_should_return_X() {
+    	$this->getRoman(10)->shouldBe("X");
+    }
+
+    public function it_should_return_XV() {
+    	$this->getRoman(15)->shouldBe("XV");
+    }
+
+    public function it_should_return_XXX() {
+    	$this->getRoman(30)->shouldBe("XXX");
+    }
+
+    public function it_should_return_C() {
+    	$this->getRoman(100)->shouldBe("C");
+    }
+
+    public function it_should_return_MMCX() {
+    	$this->getRoman(2110)->shouldBe("MMCX");
+    }
+
+    public function it_should_return_CMXCIX() {
+    	$this->getRoman(999)->shouldBe("CMXCIX");
+    }
+
+    public function it_should_return_MMMCMXCIX() {
+    	$this->getRoman(3999)->shouldBe("MMMCMXCIX");
+    }
+}
---
+++ src/Markdown.php
@@ -0,0 +1,47 @@
+<?php
+class Markdown {
+	public static function getRoman($arabic) {
+		if ($arabic > 3999) {
+			return false;
+		}
+
+		$prepare = [
+			// единицы
+			["I", "V"],
+			// десятки
+			["X", "L"],
+			// сотни
+			["C", "D"],
+			// тысячи
+			["M"],
+		];
+
+		$roman = null;
+
+		$arabic = (string) $arabic;
+		for ($i = 0, $count = strlen($arabic); $i < $count; $i++) {
+			$letter = (integer) substr($arabic, -(1 + $i), 1);
+
+			// [0, 3]
+			if ($letter <= 3) {
+				$plus = str_repeat($prepare[$i][0], $letter);
+			}
+			// [4, 5]
+			else if ($letter < 5) {
+				$plus = str_repeat($prepare[$i][0], 5 - $letter) . $prepare[$i][1];
+			}
+			// [6, 8]
+			else if ($letter <= 8) {
+				$plus = $prepare[$i][1] . str_repeat($prepare[$i][0], $letter - 5);
+			}
+			// [9]
+			else {
+				$plus = $prepare[$i][0] . $prepare[$i + 1][0];
+			}
+
+			$roman = $plus . $roman;
+		}
+
+		return $roman;
+	}
+}
